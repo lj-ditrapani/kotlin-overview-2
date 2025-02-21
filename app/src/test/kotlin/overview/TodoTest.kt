911112ded3f23c8ab84c2609a401942bdc34fef4
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2025-02-21 01:03:28 -0500

    ci: test github action

--- app/src/test/kotlin/overview/TodoTest.kt
+++ app/src/test/kotlin/overview/TodoTest.kt
@@ -36,7 +36,7 @@ class TodoTest {
             todo.dispatch("add eat lunch")
             val expected =
                 listOf(
-                    Item("wash ca", State.TODO),
+                    Item("wash car", State.TODO),
                     Item("eat lunch", State.TODO),
                 )
             resultIsList(expected, todo.dispatch("list"))
